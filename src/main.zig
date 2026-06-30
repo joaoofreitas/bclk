abed3affe4c43ea66b5cf44f717099eb2b0964f3
joaoofreitas <joaoofreitas@protonmail.com>
2026-06-30 22:37:11 +0100

    add time

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,9 @@ const bclk = @import("bclk");
 const std = @import("std");
 const rl = @import("raylib");
 
-pub fn main() !void {
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
     const screen_width = 450;
     const screen_height = 450;
 
@@ -12,9 +14,12 @@ pub fn main() !void {
 
     rl.setTargetFPS(60);
 
+
     // Main game loop
     while (!rl.windowShouldClose()) {
         // TODO: Get current time and convert to binary
+        const timestamp = std.Io.Clock.real.now(io);
+        std.debug.print("Current Time: {d}\n", .{timestamp});
 
         rl.beginDrawing();
         defer rl.endDrawing();
